50a1ff9cb276504d91575378ff63b0088b107bc3
David Amato <daveamato@live.com>
2018-08-23 08:19:43 -0400

    https enforce

--- src/main/java/com/sportspf/redirect/RedirectApplication.java
+++ src/main/java/com/sportspf/redirect/RedirectApplication.java
@@ -12,3 +12,14 @@ public class RedirectApplication {
         SpringApplication.run(RedirectApplication.class, args);
     }
 }
+
+@Configuration
+public class WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.requiresChannel()
+      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+      .requiresSecure();
+  }
+}
