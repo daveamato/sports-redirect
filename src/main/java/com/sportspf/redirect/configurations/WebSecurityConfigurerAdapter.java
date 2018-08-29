37f754ceb3ac5a0c68a8ecfc887f615ca33b46cf
David Amato <daveamato@live.com>
2018-08-28 22:10:24 -0400

    added force https

---
+++ src/main/java/com/sportspf/redirect/configurations/WebSecurityConfigurerAdapter.java
@@ -0,0 +1,10 @@
+@Configuration
+public class WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.requiresChannel()
+      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+      .requiresSecure();
+  }
+}
