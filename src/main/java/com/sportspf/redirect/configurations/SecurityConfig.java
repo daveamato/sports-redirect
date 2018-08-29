99f835862e5c48aa11ccba813b05609881eaa57b
David Amato <daveamato@live.com>
2018-08-29 18:21:49 -0400

    test

--- src/main/java/com/sportspf/redirect/configurations/SecurityConfig.java
+++ src/main/java/com/sportspf/redirect/configurations/SecurityConfig.java
@@ -1,18 +1,23 @@
 package com.sportspf.redirect.configurations;
 
- 
-import org.springframework.context.annotation.Configuration; 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.HttpSecurity;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder; 
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity; 
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration; 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity; 
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter; 
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity; 
- 
+import org.springframework.security.config.annotation.web.servlet.configuration.ComponentScan;
+
 @Configuration 
-@EnableWebMvcSecurity 
-public class SecurityConfig extends WebSecurityConfigurerAdapter { 
- 
+@EnableWebMvcSecurity
+@ComponentScan({"com.sportspf.redirect"})
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  
+  
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.requiresChannel()
