a15d50d17a772fc6dc5315c72b9d35f791c5f903
David Amato <daveamato@live.com>
2018-08-30 13:12:39 -0400

    fix

--- src/main/java/com/sportspf/redirect/configurations/SecurityConfig.java
+++ src/main/java/com/sportspf/redirect/configurations/SecurityConfig.java
@@ -9,10 +9,13 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvcSecurity;
+import org.springframework.web.servlet.config.annotation.WebSecurityConfigurerAdapter;
+import org.apache.http.client.HttpClient;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 @Configuration
-@EnableMvcSecurity
+@EnableWebMvcSecurity
 @ComponentScan({"com.sportspf.redirect"})
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
   
